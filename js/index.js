c9cb4f0c15712df93891479aca46ca7de2747363
tiago ferreira <tf134926@gmail.com>
2024-10-08 11:59:07 -0300

    homi

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const btn_open_menu = document.getElementById("btn_open_menu");
+const list_menu = document.getElementById("list_menu");
+
+var active = false;
+
+btn_open_menu.addEventListener("click",()=>{
+    if(active === false){
+        list_menu.style.display="flex";
+        active=true;
+    }else{
+        list_menu.style.display="none";
+        active=false;
+    }
+});
